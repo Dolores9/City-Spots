f195cd162a81d4db05828cf2186a6d1cbef6bf46
dolores3 <dolores@atom.nl>
2025-11-23 20:53:46 +0100

    Fixed layout + comments

---
+++ app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from 'expo-router';
+import { ThemeProviderWrapper, ThemeContext } from '@/contexts/ThemeContext';
+import { useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+function InnerLayout() {
+    //Get current theme
+    const { theme } = useContext(ThemeContext);
+
+    return (
+        <>
+            //Dynamic background based on theme
+            <Stack 
+                screenOptions={{ 
+                    headerStyle: { backgroundColor: theme.colors.background }
+                }} 
+            />
+            <StatusBar style={theme.dark ? 'light' : 'dark'} />
+        </>
+    );
+}
+
+export default function RootLayout() {
+    return (
+        //Make sure every screen can use theme. 
+        <ThemeProviderWrapper>
+            <InnerLayout />
+        </ThemeProviderWrapper>
+    );
+}
